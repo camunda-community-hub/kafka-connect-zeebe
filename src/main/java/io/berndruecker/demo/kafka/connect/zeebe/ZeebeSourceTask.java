e586758472d91565dabacf9ea759524816012429
Bernd Rücker <bernd.ruecker@camunda.com>
2019-05-08 17:16:37 +0200

    Switched from BYTE_ARRAY to STRING for Kafka message, small refactoring of variables passing

--- src/main/java/io/berndruecker/demo/kafka/connect/zeebe/ZeebeSourceTask.java
+++ src/main/java/io/berndruecker/demo/kafka/connect/zeebe/ZeebeSourceTask.java
@@ -1,17 +1,23 @@
 package io.berndruecker.demo.kafka.connect.zeebe;
 
-import java.nio.charset.Charset;
 import java.time.Duration;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import net.minidev.json.JSONValue;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.api.clients.JobClient;
@@ -19,14 +25,15 @@ import io.zeebe.client.api.response.ActivatedJob;
 import io.zeebe.client.api.subscription.JobHandler;
 import io.zeebe.client.api.subscription.JobWorker;
 
-final class JobRecord {
-  Long jobKey;
-  String variables;
-}
-
 public final class ZeebeSourceTask extends SourceTask {
-
+  
+  private class JobRecord {
+    Long jobKey;
+    String variablesJson;
+  } 
+  
   private static final Logger LOG = LoggerFactory.getLogger(ZeebeSourceTask.class);
+  private static String NAME_variablesToSendToKafka = "variablesToSendToKafka";  
 
   private String[] kafkaTopics;
 
@@ -54,29 +61,13 @@ public final class ZeebeSourceTask extends SourceTask {
     subscription = zeebe.newWorker() //
         .jobType("sendMessage") //
         .handler(new JobHandler() {
+
           public void handle(JobClient jobClient, ActivatedJob jobEvent) {
             JobRecord jobRecord = new JobRecord();
-
-            final String variablesToSendToKafka = "variablesToSendToKafka";
-            Map<String, Object> headers = jobEvent.getCustomHeaders();
-
-            if (headers.containsKey(variablesToSendToKafka)) {
-              final Map<String, Object> variablesToSend = new HashMap<>();
-
-              List<String> variablesThatShouldBeSent = Arrays.asList(
-                      headers.get(variablesToSendToKafka).toString()
-                              .trim().split("\\s*,\\s*"));
-              Map<String, Object> variables = jobEvent.getVariablesAsMap();
-              variables.forEach((k,v) -> {
-                if (variablesThatShouldBeSent.indexOf(k) != -1) variablesToSend.put(k, v);
-              });
-              jobRecord.variables = JSONValue.toJSONString(variablesToSend);
-            } else {
-              jobRecord.variables = jobEvent.getVariables();
-            }
             jobRecord.jobKey = jobEvent.getKey();
+            jobRecord.variablesJson = getVariableJsonToPassToKafka(jobEvent);            
             collectedJobs.add(jobRecord);
-          }
+          }          
         }) //
         .name("KafkaConnector") //
         .timeout(Duration.ofSeconds(5)) // lock it for 5 seconds - commit (see below) must be within that time frame
@@ -84,6 +75,28 @@ public final class ZeebeSourceTask extends SourceTask {
     LOG.info("Subscribed to Zeebe at '" + zeebeBrokerAddress + "' for sending records");
   }
 
+  private String getVariableJsonToPassToKafka(ActivatedJob jobEvent) {
+    if (jobEvent.getCustomHeaders().containsKey(NAME_variablesToSendToKafka)) {
+      List<String> variableNamesThatShouldBeSent = Arrays.asList( //
+          jobEvent.getCustomHeaders().get(NAME_variablesToSendToKafka) //
+            .toString().trim().split("\\s*,\\s*"));
+      
+      final Map<String, Object> variableMap = jobEvent.getVariablesAsMap();              
+      final Map<String, Object> variablesToSend = new HashMap<>();
+      variableNamesThatShouldBeSent.forEach((variableName) -> {
+        variablesToSend.put(variableName, variableMap.get(variableName));
+      });
+      
+      try {
+        return new ObjectMapper().writeValueAsString(variablesToSend);
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException("Could not transform variables map to JSON: " + e.getMessage(), e);
+      }
+    } else {
+      return jobEvent.getVariables();
+    }
+  }
+  
   @Override
   public List<SourceRecord> poll() {
     final List<SourceRecord> records = new LinkedList<>();
@@ -92,10 +105,12 @@ public final class ZeebeSourceTask extends SourceTask {
     while ((collectedJob = collectedJobs.poll()) != null) {
 
       for (String topic : kafkaTopics) {
-        byte[] variables = collectedJob.variables.getBytes(Charset.forName("UTF-8"));
+        // You could also send a byte array instead
+        // byte[] variables = collectedJob.variablesJson.getBytes(Charset.forName("UTF-8"));
+        // Schema.BYTE_SCHEMA,
         final SourceRecord record = new SourceRecord(null, null, topic, // ignore partitions for now random.nextInt(kafkaPartitions), 
-            Schema.BYTES_SCHEMA,
-            variables
+            Schema.STRING_SCHEMA,
+            collectedJob.variablesJson
         );
         records.add(record);
         // remember job key to complete it during commit
